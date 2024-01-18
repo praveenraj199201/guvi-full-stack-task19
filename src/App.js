d0e321b18defbee6a5ad7aa70a30f69f502d6045
praveenraj199201 <111525665+praveenraj199201@users.noreply.github.com>
2024-01-18 05:15:22 -0800

    task19

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes,Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Edit from './pages/Edit';
+import Add from './pages/Add';
+
+function App() {
+  return (
+    <div className="container">
+      <Routes>
+        <Route path='/' Component={Home}></Route>
+        <Route path='/Edit/:id' Component={Edit}></Route>
+        <Route path='/add' Component={Add}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
